274233c22ba2168d36d32af0f06c8d5a3d89ecd7
Pascallis <pascallis.henoch.herutomo-2020@vokasi.unair.ac.id>
2021-09-21 23:53:24 +0700

    Menambah view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>Kel4PWBF | Home </title>
+    <title>Kel4PWBF | Home1 </title>
 </head>
 <body>
     <h1>Halaman Home</h1>
