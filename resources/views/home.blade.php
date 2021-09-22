7a8e72ba9332c6bf5a849820922b972a13022584
Pascallis <pascallis.henoch.herutomo-2020@vokasi.unair.ac.id>
2021-09-22 07:35:36 +0700

    Edit home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>Kel4PWBF | Home1 </title>
+    <title>Kel4PWBF | Home </title>
 </head>
 <body>
-    <h1>Halaman Home1</h1>
+    <h1>Halaman Home</h1>
 
 
     <script src="js/script.js"></script>
